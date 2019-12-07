a6a4049044edc56786000d0317af80769ca4ef1a
shangmingchao <smchao1993@163.com>
2019-12-07 19:11:29 +0800

    Fix test

--- app/src/androidTest/java/com/frank/han/MainActivityBehaviorUiTest.kt
+++ app/src/androidTest/java/com/frank/han/MainActivityBehaviorUiTest.kt
@@ -3,15 +3,23 @@ package com.frank.han
 import android.content.Context
 import android.content.Intent
 import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.ComponentNameMatchers
+import androidx.test.espresso.intent.matcher.IntentMatchers
+import androidx.test.espresso.intent.matcher.IntentMatchers.toPackage
+import androidx.test.espresso.intent.rule.IntentsTestRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SdkSuppress
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.uiautomator.By
 import androidx.test.uiautomator.UiDevice
 import androidx.test.uiautomator.Until
+import org.hamcrest.Matchers.allOf
 import org.hamcrest.core.IsNull.notNullValue
 import org.junit.Assert.assertThat
 import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 
 /**
@@ -30,6 +38,9 @@ class MainActivityBehaviorUiTest {
 
     private lateinit var device: UiDevice
 
+    @get:Rule
+    val intentsTestRule = IntentsTestRule<MainActivity>(MainActivity::class.java)
+
     @Before
     fun start() {
         device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
@@ -43,5 +54,19 @@ class MainActivityBehaviorUiTest {
         }
         context.startActivity(intent)
         device.wait(Until.hasObject(By.pkg(PACKAGE).depth(0)), LAUNCH_TIMEOUT)
+    }
+
+    @Test
+    fun testRepoRouter() {
+        val repoBtn = device.findObject(By.res(PACKAGE, "repo"))
+        repoBtn?.click()
+        intended(
+            allOf(
+                IntentMatchers.hasComponent(
+                    ComponentNameMatchers.hasShortClassName(".ui.repo.RepoActivity")
+                ),
+                toPackage(PACKAGE)
+            )
+        )
     }
 }
