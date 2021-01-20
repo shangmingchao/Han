d699edb2b85abaddd6048725dd673ba84e1dc5d1
shangmingchao <smchao1993@163.com>
2021-01-20 15:14:06 +0800

    Fix CI

--- app/src/androidTest/java/com/frank/han/ui/repo/RepoFragmentTest.kt
+++ app/src/androidTest/java/com/frank/han/ui/repo/RepoFragmentTest.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.lifecycle.Lifecycle
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.withContentDescription
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
